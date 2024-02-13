969fee2dcdd1eff781388d641e4675b58ce88796
Danil <danil.rutkovskiy@mail.ru>
2024-02-13 17:20:57 +0300

    L6T1T2

---
+++ Part4/Lesson6/Task1/Task1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+
+int simple_string_hash(std::string str) {
+    int ret = 0;
+    for (int i = 0; i < str.size(); i++)
+        ret += static_cast<int>(str[i]);
+
+    return ret;
+}
+
+int main()
+{
+    std::string str{""};
+    while (str != "exit") {
+        std::cout << "Input a string: ";
+        std::getline(std::cin, str);
+        std::cout << "String hash: " << simple_string_hash(str) << std::endl;
+    }
+
+    return 0;
+}
---
+++ Part4/Lesson6/Task2/Task2.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+
+int real_string_hash(std::string str, int p, int n) {
+    long long int ret = 0;
+    for (int i = 0; i < str.size(); i++)
+        ret += static_cast<int>(str[i]) * pow(p, i);
+
+    return ret % n;
+}
+
+int main()
+{
+    std::string str{ "" };
+    int p = 0;
+    int n = 0;
+    std::cout << "Input p: ";
+    std::cin >> p;
+    std::cout << "Input n: ";
+    std::cin >> n;
+    std::cin.ignore();
+    while (str != "exit") {
+        std::cout << "Input a string: ";
+        std::getline(std::cin, str);
+        std::cout << "String hash: " << real_string_hash(str, p, n) << std::endl;
+    }
+
+    return 0;
+}
