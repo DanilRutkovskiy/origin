59a05240bc1e483c0a9adc8a64a74125533f5ae5
Danil <danil.rutkovskiy@mail.ru>
2024-03-08 16:01:01 +0300

    P5L5T1

---
+++ Part5/Lesson5/Task1/Task1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <array>
+#include <string>
+#include <utility>
+#include <algorithm>
+#include <map>
+#include <vector>
+
+int main()
+{
+    std::string str;
+    std::getline(std::cin, str);
+
+    std::map<char, int> map;
+
+    for (const auto& el : str)
+        map[el]++;
+
+    std::vector<std::pair<char, int>> vec{map.begin(), map.end()};
+
+    std::sort(vec.begin(), vec.end(), [](std::pair<char, int> p1, std::pair<char, int> p2) {
+        return p1.second > p2.second;
+        });
+
+    for (const auto& el : vec)
+        std::cout << el.first << ": " << el.second << std::endl;
+
+    return 0;
+}
