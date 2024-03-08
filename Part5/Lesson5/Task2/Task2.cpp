7c22611ca962ecd74e7a607e679b3f0cc0c218d1
Danil <danil.rutkovskiy@mail.ru>
2024-03-08 16:14:38 +0300

    P5L5T2

---
+++ Part5/Lesson5/Task2/Task2.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+#include <set>
+#include <algorithm>
+#include <vector>
+
+int main()
+{
+    std::set<int> st;
+    int count;
+    std::cout << "IN:" << std::endl;
+    std::cin >> count;
+
+    while (count > 0) {
+        int val;
+        std::cin >> val;
+        st.insert(val);
+        --count;
+    }
+
+    std::vector<int> vec{ st.begin(), st.end() };
+
+    std::sort(vec.begin(), vec.end(), [](int el1, int el2) {
+        return el1 > el2;
+        });
+
+    std::cout << "OUT" << std::endl;
+    for (const auto& el : vec)
+        std::cout << el << std::endl;
+
+    return 0;
+}
