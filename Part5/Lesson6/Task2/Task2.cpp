7a92adc9a8f24cee4e3452b5416a2ad6959735fd
Danil <danil.rutkovskiy@mail.ru>
2024-03-14 19:02:11 +0300

    P6L6T2

---
+++ Part5/Lesson6/Task2/Task2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <map>
+#include <vector>
+#include <set>
+#include <iterator>
+#include <list>
+
+template<typename Iterator>
+void print_container(Iterator begin, Iterator end) {
+	while (begin != end) {
+		std::cout << *begin << " ";
+		std::advance(begin, 1);
+	}
+}
+
+int main()
+{
+	std::vector<int> vec{1, 2, 3, 4, 5} ;
+	std::cout << "Vector: ";
+	print_container(vec.begin(), vec.end());
+
+	std::set<int> set{ 5, 4, 3, 2, 1 };
+	std::cout << std::endl << "Set: ";
+	print_container(set.begin(), set.end());
+
+	std::list<int> list{ 4, 7, 8, 9, 112 };
+	std::cout << std::endl << "List: ";
+	print_container(list.begin(), list.end());
+	return 0;
+}
