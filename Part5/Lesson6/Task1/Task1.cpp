5c5ac6d88d925104b60c16d6027576f342622b02
Danil <danil.rutkovskiy@mail.ru>
2024-03-14 18:21:26 +0300

    P5L6T1

---
+++ Part5/Lesson6/Task1/Task1.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+#include <map>
+
+template <typename T>
+void remove_dublicates(std::vector<T>& vec) {
+	std::map<T, int> map;
+
+	for (const auto& elem : vec)
+		map[elem]++;
+
+	vec.clear();
+
+	vec.reserve(map.size());
+	
+	for (const auto& elem : map)
+		vec.push_back(elem.first);
+}
+
+
+int main()
+{
+	std::vector<int> vec{ 1,1,2,3,4,5,6,4,6,7 };
+
+	std::cout << "Initial vector: ";
+	for (const auto& elem : vec)
+		std::cout << elem << " ";
+
+	remove_dublicates(vec);
+
+	std::cout << std::endl;
+
+	std::cout << "After removing dublicates: ";
+	for (const auto& elem : vec)
+		std::cout << elem << " ";
+}
