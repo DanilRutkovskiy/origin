2f8b8bd94a98e82de2ffb0ad634509b598a3b0c8
Danil <rutkovskiy.danil@mail.ru>
2024-04-28 05:33:09 +0300

    Remove build directore

--- Part6 - SQL/Lesson5/out/build/x64-Debug/ClientManager/libpqxx_build/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/cpp_libs/libpqxx_7.9.0/test
-# Build directory: D:/Repo/origin/Part6 - SQL/Lesson5/out/build/x64-Debug/ClientManager/libpqxx_build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(runner "D:/Repo/origin/Part6 - SQL/Lesson5/out/build/x64-Debug/ClientManager/libpqxx_build/test/runner.exe")
-set_tests_properties(runner PROPERTIES  _BACKTRACE_TRIPLES "D:/cpp_libs/libpqxx_7.9.0/test/CMakeLists.txt;12;add_test;D:/cpp_libs/libpqxx_7.9.0/test/CMakeLists.txt;0;")
