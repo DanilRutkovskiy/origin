cb2c43daa607f6399060cb34afa92b2690d94703
Danil.Rutkovskiy <rutkovskiy.danil@mail.ru>
2024-07-17 14:48:05 +0500

    Fix

--- Part9/Lesson1/Task1/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Task1_FILE D:/repo/origin/Part9/Lesson1/Task1/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/Task1.exe)
-set(__QT_DEPLOY_TARGET_Task1_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Task1_RUNTIME_DLLS D:/libs/Qt/6.7.2/mingw_64/bin/Qt6Network.dll;D:/libs/Qt/6.7.2/mingw_64/bin/Qt6Sql.dll;D:/libs/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
